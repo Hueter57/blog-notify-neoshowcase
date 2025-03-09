e5ec2d1b077bcae87509d45d8e61954a846915f3
Hueter <kohsukemori@gmail.com>
2025-03-10 00:21:07 +0900

    🍞 add hubot-traq.d.ts

---
+++ scripts/tsScripts/hubot-traq.d.ts
@@ -0,0 +1,71 @@
+// Type definitions for hubot
+// Definitions by: Hueter
+
+type SendChannel = {
+  channelID: string;
+};
+
+type SendDM = {
+  userlID: string;
+};
+
+declare module "hubot" {
+  export interface Robot {
+    listen(
+      regex: RegExp,
+      options: { [key: string]: any } | Function,
+      callback?: (res: Response) => void
+    ): void;
+    hear(
+      regex: RegExp,
+      options: { [key: string]: any } | Function,
+      callback?: (res: Response) => void
+    ): void;
+    respond(
+      regex: RegExp,
+      options: { [key: string]: any } | Function,
+      callback?: (res: Response) => void
+    ): void;
+    send(user: string, sendChannel?: SendChannel, sendDM?: SendDM, ...string: string[]): void;
+    reply(user: string, ...string: string[]): void;
+  }
+
+  export interface Response {
+    match: [string, string, { index: number }, { input: string }];
+
+    message: {
+      id: string;
+      user: {
+        id: string;
+        name: string;
+        displayName: string;
+        iconID: string;
+        bot: boolean;
+      };
+      room: {
+        type: string;
+        id: string;
+      };
+      text: string;
+      plainText: string;
+      embedded: [
+        {
+          raw: string;
+          type: string;
+          id: string;
+        }
+      ];
+      createdAt: Date;
+      updatedAt: Date;
+    };
+
+    send(...strings: string[]): void;
+    emote(...strings: string[]): void;
+    reply(...strings: string[]): void;
+    topic(...strings: string[]): void;
+    play(...strings: string[]): void;
+    locked(...strings: string[]): void;
+    random(items: any[]): any;
+    finish(): void;
+  }
+}
