bbd0e348e59abf7a962c8e7a4d9d4fb0053d1fb5
Hueter <kohsukemori@gmail.com>
2025-03-10 00:23:25 +0900

    🍞 add corn.ts

---
+++ scripts/tsScripts/corn.ts
@@ -0,0 +1,58 @@
+// Description:
+//
+// Commands:
+//
+
+import hubot from "hubot";
+import { calcDateDiff, getMessages } from "./schedule";
+import { envData } from "./init";
+
+const corn = require("node-cron");
+
+module.exports = (robot: hubot.Robot): void => {
+  startCorn(robot);
+};
+
+function startCorn(robot: hubot.Robot): void {
+  if (!envData.validData) {
+    return;
+  }
+  const { crowi, blogRelay, noticeMessage } = envData;
+  const dateDiff = calcDateDiff(blogRelay);
+  if (dateDiff >= blogRelay.days) {
+    console.log("blogRelay have already ended");
+    return;
+  }
+  const mainCorn = corn.schedule(
+    "0 0 8 * * *",
+    async () => {
+      const messages = await getMessages(crowi, blogRelay, noticeMessage);
+      if (messages.length === 0) {
+        return;
+      }
+      console.log(messages);
+      // robot.send(messages[0], { channelID: envData.traQ.channelId });
+      robot.send(messages[1], { channelID: envData.traQ.logChannelId });
+    },
+    {
+      Scheduled: true,
+      timezone: "Asia/Tokyo",
+    }
+  );
+}
+
+function getCornScheduleString(date: Date): string {
+  const second = date.getSeconds();
+  const minute = date.getMinutes();
+  const hour = date.getHours();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  return second + " " + minute + " " + hour + " " + day + " " + month + " *";
+}
+
+export function JapaneseDate(date?: string): Date {
+  const dateMs =
+    date === undefined ? new Date().getTime() : new Date(date).getTime();
+  const offsetMs = 9 * 60 * 60 * 1000; // 9hours
+  return new Date(dateMs + offsetMs);
+}
