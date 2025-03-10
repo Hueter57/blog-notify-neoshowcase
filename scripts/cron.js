c69afa59de576a1f6999d14f178f655131eaa9a4
Hueter <kohsukemori@gmail.com>
2025-03-10 23:14:14 +0900

    🍞 add manual cron control

--- scripts/cron.js
+++ scripts/cron.js
@@ -3,25 +3,57 @@
 //
 // Commands:
 //
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.JapaneseDate = JapaneseDate;
 const schedule_1 = require("./schedule");
 const init_1 = require("./init");
-const cron = require("node-cron");
+const node_cron_1 = __importDefault(require("node-cron"));
 module.exports = (robot) => {
-    startCron(robot);
+    let mainCorn = startCron(robot);
+    robot.hear(/cronStart$/i, async (res) => {
+        if (mainCorn === null) {
+            mainCorn = startCron(robot);
+            if (mainCorn !== null) {
+                console.log("new corn start");
+                res.send("new cron Start");
+            }
+            else {
+                console.log("cronStart failed");
+                res.send("cronStart failed");
+            }
+        }
+        else {
+            mainCorn.start();
+            console.log("cron start");
+            res.send("corn start");
+        }
+    });
+    robot.hear(/cronStop$/i, async (res) => {
+        if (mainCorn !== null) {
+            mainCorn.stop();
+            console.log("cron stop");
+            res.send("cron stop");
+        }
+        else {
+            console.log("cron is null");
+            res.send("cron is null");
+        }
+    });
 };
 function startCron(robot) {
     if (!init_1.envData.validData) {
-        return;
+        return null;
     }
     const { crowi, blogRelay, noticeMessage } = init_1.envData;
     const dateDiff = (0, schedule_1.calcDateDiff)(blogRelay);
     if (dateDiff >= blogRelay.days) {
         console.log("blogRelay have already ended");
-        return;
+        return null;
     }
-    const mainCron = cron.schedule("0 0 8 * * *", async () => {
+    let mainCron = node_cron_1.default.schedule("0 0 8 * * *", async () => {
         const messages = await (0, schedule_1.getMessages)(crowi, blogRelay, noticeMessage);
         if (messages.length === 0) {
             return;
@@ -30,10 +62,11 @@ function startCron(robot) {
         robot.send({ channelID: init_1.envData.traQ.channelId }, messages[0]);
         robot.send({ channelID: init_1.envData.traQ.logChannelId }, messages[1]);
     }, {
-        Scheduled: true,
+        scheduled: true,
         timezone: "Asia/Tokyo",
     });
     console.log("start cron");
+    return mainCron;
 }
 function getCronScheduleString(date) {
     const second = date.getSeconds();
--- scripts/tsScripts/cron.ts
+++ scripts/tsScripts/cron.ts
@@ -6,24 +6,51 @@
 import hubot from "hubot";
 import { calcDateDiff, getMessages } from "./schedule";
 import { envData } from "./init";
-
-const cron = require("node-cron");
+import cron from "node-cron";
 
 module.exports = (robot: hubot.Robot): void => {
-  startCron(robot);
+  let mainCorn = startCron(robot);
+
+  robot.hear(/cronStart$/i, async (res: hubot.Response): Promise<void> => {
+    if (mainCorn === null) {
+      mainCorn = startCron(robot);
+      if (mainCorn !== null) {
+        console.log("new corn start");
+        res.send("new cron Start");
+      } else {
+        console.log("cronStart failed");
+        res.send("cronStart failed");
+      }
+    } else {
+      mainCorn.start();
+      console.log("cron start");
+      res.send("corn start");
+    }
+  });
+
+  robot.hear(/cronStop$/i, async (res: hubot.Response): Promise<void> => {
+    if (mainCorn !== null) {
+      mainCorn.stop();
+      console.log("cron stop");
+      res.send("cron stop");
+    } else {
+      console.log("cron is null");
+      res.send("cron is null");
+    }
+  });
 };
 
-function startCron(robot: hubot.Robot): void {
+function startCron(robot: hubot.Robot): cron.ScheduledTask | null {
   if (!envData.validData) {
-    return;
+    return null;
   }
   const { crowi, blogRelay, noticeMessage } = envData;
   const dateDiff = calcDateDiff(blogRelay);
   if (dateDiff >= blogRelay.days) {
     console.log("blogRelay have already ended");
-    return;
+    return null;
   }
-  const mainCron = cron.schedule(
+  let mainCron = cron.schedule(
     "0 0 8 * * *",
     async () => {
       const messages = await getMessages(crowi, blogRelay, noticeMessage);
@@ -35,11 +62,12 @@ function startCron(robot: hubot.Robot): void {
       robot.send({ channelID: envData.traQ.logChannelId }, messages[1]);
     },
     {
-      Scheduled: true,
+      scheduled: true,
       timezone: "Asia/Tokyo",
     }
   );
   console.log("start cron");
+  return mainCron;
 }
 
 function getCronScheduleString(date: Date): string {
