cbe829590b800c520430f16790d194a3c359712b
Hueter <kohsukemori@gmail.com>
2025-08-02 00:37:25 +0900

    🍞 add help command

--- scripts/tsScripts/main.ts
+++ scripts/tsScripts/main.ts
@@ -10,6 +10,7 @@ module.exports = (robot: hubot.Robot): void => {
   robot.respond(/ping$/i, async (res: hubot.Response): Promise<void> => {
     await res.reply("pong");
   });
+
   robot.hear(/checkEnvData$/i, async (res: hubot.Response): Promise<void> => {
     const envStatusList = await checkEnvData();
     const envStatusMessage = envStatusList
@@ -20,5 +21,14 @@ module.exports = (robot: hubot.Robot): void => {
 ${envStatusMessage}`;
     console.log(message);
     res.send(message);
+  });
+
+  robot.respond(/help$/i, async (res: hubot.Response): Promise<void> => {
+    await res.reply(`コマンド一覧\n
+- \`ping\`: BOTの稼働確認\n
+- \`help\`: ヘルプを表示\n
+- \`checkEnvData\`: 環境変数の確認\n
+- \`cronStart\`: cronの開始 (環境変数変更後一度\`checkEnvData\`の実行が必要)\n
+- \`cronStop\`: cronの停止`);
   });
 };
