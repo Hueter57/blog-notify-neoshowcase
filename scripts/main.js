cf9074f5d1d59e3f4f8edcfb95b6b4d985346c9c
Hueter <kohsukemori@gmail.com>
2025-08-02 00:52:00 +0900

    🍥 fix main.js from main.ts

--- scripts/main.js
+++ scripts/main.js
@@ -20,4 +20,12 @@ ${envStatusMessage}`;
         console.log(message);
         res.send(message);
     });
+    robot.respond(/help$/i, async (res) => {
+        await res.reply(`コマンド一覧\n
+- \`ping\`: BOTの稼働確認\n
+- \`help\`: ヘルプを表示\n
+- \`checkEnvData\`: 環境変数の確認\n
+- \`cronStart\`: cronの開始 (環境変数変更後一度\`checkEnvData\`の実行が必要)\n
+- \`cronStop\`: cronの停止`);
+    });
 };
