fa4b3a1116da332a59083682b63749af8d173d91
KohsukeMori <kohsukemori@gmail.com>
2025-12-09 01:44:48 +0900

    fix: providerをprisma-clientに変更

--- scripts/tsScripts/lib/prisma.ts
+++ scripts/tsScripts/lib/prisma.ts
@@ -1,12 +1,19 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined;
 };
 
-export const prisma =
-    globalForPrisma.prisma ??
-    new PrismaClient();
+const adapter = new PrismaMariaDb({
+    host: process.env.NS_MARIADB_HOSTNAME,
+    user: process.env.NS_MARIADB_USER,
+    password: process.env.NS_MARIADB_PASSWORD,
+    database: process.env.NS_MARIADB_DATABASE,
+    port: process.env.NS_MARIADB_PORT ? parseInt(process.env.NS_MARIADB_PORT) : 3306,
+    connectionLimit: 5
+});
+export const prisma = new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') {
     globalForPrisma.prisma = prisma;
